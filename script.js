9c1cbd9caaa478c65148550fb04c15cb416aff1a
Mohammed Abdul Moheet <113626576+CodeWithMoheet@users.noreply.github.com>
2023-08-05 20:39:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// let string = ""
+// let buttons = document.querySelectorAll(".button");
+// Array.from(buttons).forEach((button) => {
+//     button.addEventListener('click', (e) => {
+       
+//         if (e.target.innerHTML == "=") {
+//             string = eval(string)
+//             document.querySelector("input").value = string;
+//         }
+//          else if (e.target.innerHTML == "AC") {
+//             string = ""
+//             document.querySelector("input").value = string;
+//         } else {
+//             console.log(e.target.innerHTML);
+//             string = string + e.target.innerHTML;
+//             console.log(string)
+//             document.querySelector('input').value = string;
+//         }
+//         // button.style.background = "green"
+//         // return;
+//     })
+   
+    
+// })
